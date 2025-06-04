4b21c26aaa3829f9d31c6b704ba6cb880a041aa0
Federico Miraglia <personalmailfm98@gmail.com>
2025-06-04 23:30:25 +0200

    added btm to quick terminals

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,17 +1,17 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		version = "*",
-		config = function()
-			local toggleterm = require("toggleterm")
-			toggleterm.setup({})
-			local Terminal = require("toggleterm.terminal").Terminal
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      local toggleterm = require("toggleterm")
+      toggleterm.setup({})
+      local Terminal = require("toggleterm.terminal").Terminal
 
       local floating = Terminal:new(
         {
           direction = "float",
           float_opts = {
-            border="curved"
+            border = "curved"
           }
         }
       )
@@ -20,42 +20,71 @@ return {
         floating:toggle()
       end
 
-			local lazygit = Terminal:new({
-				cmd = "lazygit",
-				dir = "git_dir",
-				direction = "float",
-				float_opts = {
-          border="curved"
-				},
-				-- function to run on opening the terminal
-				on_open = function(term)
-					vim.cmd("startinsert!")
-					vim.api.nvim_buf_set_keymap(
-						term.bufnr,
-						"n",
-						"q",
-						"<cmd>close<CR>",
-						{ noremap = true, silent = true }
-					)
-				end,
-				-- function to run on closing the terminal
-				on_close = function(term)
-					vim.cmd("startinsert!")
-				end,
-			})
+      local btm = Terminal:new({
+        cmd = "btm",
+        dir = "git_dir",
+        direction = "float",
+        float_opts = {
+          border = "curved"
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(
+            term.bufnr,
+            "n",
+            "q",
+            "<cmd>close<CR>",
+            { noremap = true, silent = true }
+          )
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+          vim.cmd("startinsert!")
+        end,
+      })
 
-			function _lazygit_toggle()
-				lazygit:toggle()
-			end
+      function _btm_toggle()
+        btm:toggle()
+      end
+
+      local lazygit = Terminal:new({
+        cmd = "lazygit",
+        dir = "git_dir",
+        direction = "float",
+        float_opts = {
+          border = "curved"
+        },
+        -- function to run on opening the terminal
+        on_open = function(term)
+          vim.cmd("startinsert!")
+          vim.api.nvim_buf_set_keymap(
+            term.bufnr,
+            "n",
+            "q",
+            "<cmd>close<CR>",
+            { noremap = true, silent = true }
+          )
+        end,
+        -- function to run on closing the terminal
+        on_close = function(term)
+          vim.cmd("startinsert!")
+        end,
+      })
+
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
 
       local wk = require("which-key")
-      
+
       wk.add({
-        {"<leader>t", group = "Terminal"},
-        {"<leader>tt", "<cmd>ToggleTerm<cr>", desc = "Open terminal"},
-        {"<leader>tf", "<cmd>lua _floating_toggle()<cr>", desc = "Open floating terminal"},
-        {"<leader>tg", "<cmd>lua _lazygit_toggle()<cr>", desc = "Open lazygit"},
+        { "<leader>t",  group = "Terminal" },
+        { "<leader>tt", "<cmd>ToggleTerm<cr>",             desc = "Open terminal" },
+        { "<leader>tf", "<cmd>lua _floating_toggle()<cr>", desc = "Open floating terminal" },
+        { "<leader>tg", "<cmd>lua _lazygit_toggle()<cr>",  desc = "Open lazygit" },
+        { "<leader>tb", "<cmd>lua _btm_toggle()<cr>",      desc = "Open bottom" },
       })
-		end,
-	},
+    end,
+  },
 }
