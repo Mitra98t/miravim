e04c1edc4cd31c62fea286845cef4e5cb1740bb3
Federico Miraglia <personalmailfm98@gmail.com>
2025-06-30 17:43:03 +0200

    better command input with noice

--- lua/plugins/nvimdap.lua
+++ lua/plugins/nvimdap.lua
@@ -1,6 +1,5 @@
 return {
   "rcarriga/nvim-dap-ui",
-  lazy = true,
   dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
   config = function()
     local dap, dapui = require("dap"), require("dapui")
