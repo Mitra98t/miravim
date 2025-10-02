1f82c48f22c6bba115f393aaf5159e0f74f3a9af
Federico Miraglia <personalmailfm98@gmail.com>
2025-10-02 15:22:54 +0200

    new breadcrum plugin

--- lua/plugins/breadcrums.lua
+++ lua/plugins/breadcrums.lua
@@ -1,25 +1,12 @@
 return {
-  "Bekaboo/dropbar.nvim",
-  -- optional, but required for fuzzy finder support
-  lazy = true,
-  dependencies = {
-    "nvim-telescope/telescope-fzf-native.nvim",
-    build = "make",
-  },
-  config = function()
-    local dropbar_api = require("dropbar.api")
-
-    local wk = require("which-key")
-
-    wk.add({
-      { "<leader>;", group = "Breadcrums" },
-      {
-        "<leader>;;",
-        function()
-          dropbar_api.pick()
-        end,
-        desc = "Pick symbols in winbar"
-      }
-    })
-  end,
+	"utilyre/barbecue.nvim",
+	name = "barbecue",
+	version = "*",
+	dependencies = {
+		"SmiteshP/nvim-navic",
+		"nvim-tree/nvim-web-devicons", -- optional dependency
+	},
+	opts = {
+		-- configurations go here
+	},
 }
