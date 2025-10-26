7d6467c8e2d67206556d3d327ec459378bc85ec4
Federico Miraglia <personalmailfm98@gmail.com>
2025-10-26 23:32:26 +0100

    added gitsigns functionalities and rounded border to lsp documentation

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -4,9 +4,26 @@ return {
   event = { "BufReadPre", "BufNewFile" },
   config = function()
     local gs = require("gitsigns")
+    local border = vim.g.floating_border_style or "rounded"
 
     gs.setup({
       current_line_blame = true,
+      preview_config = {
+        border = border,
+      },
+    })
+
+
+    local wk = require 'which-key'
+    wk.add({
+      { "<leader>g",  group = "Git" },
+      { "<leader>gb", "<cmd>Gitsigns blame<cr>",               desc = "Show Blame of Entire File" },
+      { "<leader>gg", "<cmd>Gitsigns preview_hunk<cr>",        desc = "Preview Hunk" },
+      { "<leader>gG", "<cmd>Gitsigns preview_hunk_inline<cr>", desc = "Preview Hunk Inline" },
+      { "<leader>gn", "<cmd>Gitsigns nav_hunk next<cr>",       desc = "Goto Next Hunk" },
+      { "<leader>gN", "<cmd>Gitsigns nav_hunk prev<cr>",       desc = "Goto Prev Hunk" },
+      { "<leader>xg", "<cmd>Gitsigns setqflist<cr>",           desc = "Open quick fix list for Hunks" },
+
     })
   end,
 }
--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,9 +1,21 @@
 return {
   "folke/noice.nvim",
   event = "VeryLazy",
-  opts = {
-    -- add any options here
-  },
+  opts = function()
+    local border = vim.g.floating_border_style or "rounded"
+    return {
+      presets = {
+        lsp_doc_border = true,
+      },
+      views = {
+        hover = {
+          border = {
+            style = border,
+          },
+        },
+      },
+    }
+  end,
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
