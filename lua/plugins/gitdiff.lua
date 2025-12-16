a39930c7a070c1fb1a86cd6fa5299c04579ae653
Federico Miraglia <personalmailfm98@gmail.com>
2025-12-16 10:37:24 +0100

    added shortcut for git diff visualization

--- lua/plugins/gitdiff.lua
+++ lua/plugins/gitdiff.lua
@@ -1,5 +1,14 @@
 return {
   "esmuellert/vscode-diff.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
+  lazy = true,
+  event = { "BufReadPre", "BufNewFile" },
   cmd = "CodeDiff",
+  config = function()
+    local wk = require 'which-key'
+    wk.add({
+      { "<leader>gf", "<cmd>CodeDiff<cr>", desc = "Open Diff Visualizer" },
+
+    })
+  end,
 }
