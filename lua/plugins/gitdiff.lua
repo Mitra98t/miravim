44deaa10c78948d9562306db7f0f82bceb0f5532
Federico Miraglia <personalmailfm98@gmail.com>
2026-01-28 09:28:40 +0100

    [FEAT] diff now works with specific git rev

--- lua/plugins/gitdiff.lua
+++ lua/plugins/gitdiff.lua
@@ -8,7 +8,17 @@ return {
     local wk = require 'which-key'
     wk.add({
       { "<leader>gf", "<cmd>CodeDiff<cr>", desc = "Open Diff Visualizer" },
-
+      {
+        "<leader>gF",
+        function()
+          vim.ui.input({ prompt = "CodeDiff revision (es: HEAD, HEAD~1, main, v1.0.0): ", default = "HEAD" },
+            function(rev)
+              if not rev or rev == "" then return end
+              vim.cmd("CodeDiff file " .. rev)
+            end)
+        end,
+        desc = "CodeDiff current file vs revision"
+      },
     })
   end,
 }
