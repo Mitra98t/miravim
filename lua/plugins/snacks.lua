4432f94d70dd27792230832942f41870a1620fcd
Federico Miraglia <personalmailfm98@gmail.com>
2026-02-24 09:25:20 +0100

    [CHORE] build folder excluded from searches

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -19,7 +19,7 @@ return {
         enabled = true,
         hidden = true,
         ignored = true,
-        exclude = { "docs", "node_modules", ".git" },
+        exclude = { "docs", "node_modules", ".git", "build" },
         layout = {
           preset = "telescope",
         },
