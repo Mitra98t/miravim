e6561f8c92967bc6c1f60c346fbfdd679796c130
Federico Miraglia <personalmailfm98@gmail.com>
2026-04-10 16:15:28 +0200

    [FIX] better keymap group for ai

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -40,6 +40,7 @@ return {
         { "<C-s>", write_file,      desc = "Write File" },
         { "<C-q>", "<cmd>qall<cr>", desc = "Exit Nvim" }
       },
+      { "<leader>a", group = "AI/Claude Code" },
       { "<leader>c", group = "Code" },
       {
         { "gd",         "<cmd>FzfLua lsp_declarations<cr>", desc = "Go to declaration" },
